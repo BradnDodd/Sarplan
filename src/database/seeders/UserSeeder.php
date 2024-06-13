1d62f44ff2b9adea4f5e1b98eb7e39d7ceacd828
Your Name <you@example.com>
2024-06-13 11:35:16 +0100

    Add demo user to seeder

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +15,10 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Demo',
+            'email' => 'demo@demo.com',
+            'password' => Hash::make('demo'),
+        ]);
     }
 }
